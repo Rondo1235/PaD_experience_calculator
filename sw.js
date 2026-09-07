700eaa7476376b78bdbb8d6c079f77f45675dd9e
Rondo1235 <kd1414338@st.kobedenshi.ac.jp>
2026-09-07 13:09:18 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "exp-calculator-v2";
+const CACHE_NAME = "exp-calculator-v3";
 
 const FILES_TO_CACHE = [
     "./",
@@ -9,15 +9,32 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+    self.skipWaiting();
+
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(FILES_TO_CACHE))
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        }).then(() => {
+            return self.clients.claim();
+        })
     );
 });
 
 self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request)
+            .catch(() => caches.match(event.request))
     );
 });
